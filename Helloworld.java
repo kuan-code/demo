5db316a6db7924d8095443ab5c23632263466099
kuan-code <15188059625@163.com>
2020-02-24 21:10:05 +0800

    helloworld

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+Public String Helloworld{
+	public static void main(String[] args){
+		System.out.println("Hello world")
+		System.out.println("1")
+		System.out.println("2")
+		System.out.println("3")
+	}
+}
